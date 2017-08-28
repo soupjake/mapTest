7fa69c0e991c12b90d4d6435afdea03ab8a83c2e
SnakeyHips <super.jake@hotmail.co.uk>
2017-08-28 16:47:40 +0100

    unit tests update
    added UI espresso tests

---
+++ androidTest/java/com/asuper/maptest/ChangeInstrumentedTest.java
@@ -0,0 +1,37 @@
+package com.asuper.maptest;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ChangeInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MapsActivity> mMapsActivityActivityTestRule =
+            new ActivityTestRule<MapsActivity>(MapsActivity.class);
+
+    @Test
+    public void clickLocationButton_updatesUI() throws Exception{
+        onView(withId(R.id.mCloudButton))
+                .perform(click());
+        onView(withId(R.id.mWeatherText))
+                .check(matches(withText("32%")));
+    }
+
+}
+
